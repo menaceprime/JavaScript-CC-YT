98f626755730e54fe7e810d68979bbd570d8fbfd
menaceprime <aadarshsinghh121@gmail.com>
2025-05-13 14:14:37 +0000

    Variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1446
+let accountEmail = "acz@KeyboardEvent.com"
+var accountPassword = "0987678"
+accountCity = "Banglore"
+let accountState;
+
+// accountId = 2345 cannot change const once declared;
+
+accountEmail = "zxc2pagla.com"
+accountPassword = "123456"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
