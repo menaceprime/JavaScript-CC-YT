fda1dae76782821f664ab69ac098358eefe575a6
menaceprime <aadarshsinghh121@gmail.com>
2025-05-17 13:03:32 +0000

    datatypes discussed

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treats all JS code as newer verison
+
+//alert ( 3 + 3 )   We r using node js not browser
+
+console.log("Adarsh")
+
+/* number -> 2^53
+
+bigint
+
+String => ""
+
+boolean => true/false
+
+null => Standalone value
+
+undefined => 
+
+symbol => unique
+
+
+object 
+    
+    */
+
+console.log(typeof "Adarsh")
